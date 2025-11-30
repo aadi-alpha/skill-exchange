85daa4f47dcfa08ee218799850a482e2da9f34c1
aadi-alpha <aadinagpal46@gmail.com>
2025-11-30 13:35:49 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AuthProvider from './contextAPI/AuthContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <BrowserRouter basename="/skill-exchange">
+    <HashRouter basename="/skill-exchange">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 
 
